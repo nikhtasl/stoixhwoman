4848c900a0ae081fd75e85e1f256b7402b46f84d
nickytdrb <nikhtasoula@yahoo.com>
2023-11-12 17:38:43 +0200

    First commit

---
+++ src/main/java/com/example/stoixhwoman/StoixhwomanApplication.java
@@ -0,0 +1,13 @@
+package com.example.stoixhwoman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoixhwomanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoixhwomanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stoixhwoman/StoixhwomanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stoixhwoman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoixhwomanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
