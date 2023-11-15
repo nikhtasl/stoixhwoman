d07b68bf2db379f6aedbd86d1558cb22e1b2260a
nickytdrb <nikhtasoula@yahoo.com>
2023-11-16 01:00:53 +0200

    ApplicationTest added

--- src/test/java/com/example/stoixhwoman/StoixhwomanApplicationTests.java
+++ src/test/java/com/example/stoixhwoman/StoixhwomanApplicationTests.java
@@ -1,13 +1,39 @@
 package com.example.stoixhwoman;
 
+import com.example.stoixhwoman.controller.MatchController;
+import com.example.stoixhwoman.repository.MatchRepository;
+import com.example.stoixhwoman.service.MatchService;
+import com.example.stoixhwoman.utilities.DTOUtilities;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class StoixhwomanApplicationTests {
 
+    @Autowired
+    private MatchController matchController;
+
+    @Autowired
+    private MatchService matchService;
+
+    @Autowired
+    private MatchRepository matchRepository;
+
+    @Autowired
+    private DTOUtilities dtoUtilities;
+
     @Test
     void contextLoads() {
+        assertNotNull(matchController);
+        assertNotNull(matchService);
+        assertNotNull(matchRepository);
+        assertNotNull(dtoUtilities);
     }
 
 }
